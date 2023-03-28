f3483758d4cacaa78881f862209af5583b97def4
matthewkolden <matthewkolden2k@gmail.com>
2023-03-28 03:51:58 -0400

    Create intro

---
+++ src/components/IntroHeader.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image'
+
+export default function IntroHeader() {
+  return (
+    <header className="px-6">
+      <div className="max-w-4xl mx-auto flex gap-14 font-poppins">
+        <div>
+          <h1 className="text-3xl font-bold mb-6">
+            Hi, I'm Matthew.
+            <br />
+            I'm a Software Developer.
+          </h1>
+          <p className="text-lg mb-6">
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </p>
+          <a href="#" className="font-robotomono bg-red-400 text-white px-6 py-3 text-lg rounded-sm font-semibold">Here's my resume</a>
+        </div>
+        <span className="m-5 inline-block before:w-full before:aspect-square before:-left-1.5 before:top-1.5 before:block before:absolute before:bg-red-400 relative z-0">
+          <Image src="/profile.jpg"
+                 width="600"
+                 height="600"
+                 alt="Matt profile"
+                 className="z-10 relative" />
+        </span>
+      </div>
+    </header>
+  );
+}
